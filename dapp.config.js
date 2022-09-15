f1931446d3660b1ebda5363d5de3e4f03768af00
Tiffany Yang <tiffanyy.dev@gmail.com>
2022-09-15 00:04:16 -0400

    added fortmatic, alchemy, web3 connect, minting page, env variables

---
+++ dapp.config.js
@@ -0,0 +1,10 @@
+const config = {
+  title: 'BoredApe Dapp',
+  description: 'A BoredApe nft minting showcase',
+  contractAddress: '',
+  maxMintAmount: 20,
+  presaleMaxMintAmount: 5,
+  price: 0.01
+}
+
+export { config }
